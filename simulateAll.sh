e17246c2143ee7a8b3780d62009c169ba30c4421
pranjali-pathre <pranjali.pathre@research.iiit.ac.in>
2023-03-23 02:03:40 +0530

    Configure custom

--- simulateAll.sh
+++ simulateAll.sh
@@ -6,11 +6,11 @@ echo $tag
 
 mkdir -p saved/$tag
 
-./simulate.sh ./data/arkansaw saved/$tag/arkansaw
-./simulate.sh ./data/eudora saved/$tag/eudora
-./simulate.sh ./data/pablo saved/$tag/pablo
-./simulate.sh ./data/stokes saved/$tag/stokes
-./simulate.sh ./data/hillsdale saved/$tag/hillsdale
-./simulate.sh ./data/denmark saved/$tag/denmark
+# ./simulate.sh ./data/arkansaw saved/$tag/arkansaw
+# ./simulate.sh ./data/eudora saved/$tag/eudora
+# ./simulate.sh ./data/pablo saved/$tag/pablo
+# ./simulate.sh ./data/stokes saved/$tag/stokes
+# ./simulate.sh ./data/hillsdale saved/$tag/hillsdale
+# ./simulate.sh ./data/denmark saved/$tag/denmark
 ./simulate.sh ./data/quantico saved/$tag/quantico
-./simulate.sh ./data/mesic saved/$tag/mesic
+# ./simulate.sh ./data/mesic saved/$tag/mesic
